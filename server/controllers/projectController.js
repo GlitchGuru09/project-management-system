94a5c24d51814760af7bdb1b00f39b11b06d191d
Shrey Vernekar <glitchguru03@gmail.com>
2025-12-31 16:45:56 +0530

    fix(projectController): update updateProject function to retrieve project ID from request parameters

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -85,7 +85,6 @@ export const createProject = async (req, res) => {
 export const updateProject = async (req, res) => {
     try {
         const { userId } = await req.auth() //logged in users ID;
-        const { id } = req.params;
         const { workspaceId, name, description, status, start_date, end_date, team_members, team_lead, progress, priority } = req.body;
 
         //check if user has admin role for workspace
@@ -110,7 +109,7 @@ export const updateProject = async (req, res) => {
             }
         }
         const project = await prisma.project.update({
-            where: { id },
+            where: { id: req.params.id },
             data: {
                 workspaceId,
                 name,
